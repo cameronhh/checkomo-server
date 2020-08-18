f1dcbf0241215bdc8300b94acdd41258b96a2092
Matthew Jordan <matthew.ian.jordan@gmail.com>
2020-08-18 22:19:04 +1000

    token stuff

--- api_resources/token.py
+++ api_resources/token.py
@@ -2,8 +2,9 @@ from flask import Flask, jsonify, request, abort
 from globalspace.variables import app,config,api,db
 import model.model as model
 from flask_restplus import Resource
+import datetime
 
-from util.crypt import check_password
+from util.crypt import check_password, validate_token
 
 import jwt
 _secret = app.config.get('SECRET_KEY')
@@ -11,19 +12,27 @@ _secret = app.config.get('SECRET_KEY')
 class Token(Resource):
 
     def post(self):
-        data = request.get_json(force=True)
-        this_user = model.User.query.filter_by(email=data.get("email")).first()
-        # abort if not valid user
-        if this_user is None: abort(403)
-        # abort if password wrong/None
-        if not check_password(data.get("password"), this_user.password): abort(403)
-        # remove password hash from
-        dict_user = this_user.to_dict()
-        del dict_user['password']
+
+        auth_to_user_id = validate_token(validate_only=True)
+        # if the user isn't getting a new token
+        if not auth_to_user_id:
+            data = request.get_json(force=True)
+            this_user = model.User.query.filter_by(email=data.get("email")).first()
+            # abort if not valid user
+            if this_user is None: abort(403)
+            # abort if password wrong/None
+            if not check_password(data.get("password"), this_user.password): abort(403)
+            # remove password hash from
+            dict_user = this_user.to_dict()
+            del dict_user['password']
+            auth_to_user_id = dict_user['id']
+        else:
+            validate_token() # so we can abort properly
         
         token_dict = {
-            "user_id" : dict_user['id'],
-            "user" : dict_user
+            "user_id" : auth_to_user_id,
+            #TODO propper expirery
+            "valid_to_dttm" : str( datetime.datetime.now() + datetime.timedelta(days=14) )
             #expirery and other shit
         }
 
--- util/crypt.py
+++ util/crypt.py
@@ -13,17 +13,29 @@ def check_password(plain_text_password, hashed_password):
     return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 
-from flask import request
+from flask import request, abort
 import jwt
+import datetime
+import dateutil
 
 # validate a token and return user id or None
-def validate_token():
+def validate_token( validate_only=False ):
+
+    def abort_or_validate( code, message = None):
+        if validate_only:
+            return None
+        abort(code, message)
+
     if request.headers.get('Authorization') is None: return None
     token_str = request.headers.get('Authorization').split(" ")[1]
     try:
         decoded_token = jwt.decode(token_str.encode('utf-8'), _secret, algorithms=['HS256'])
-    except jwt.exceptions.InvalidSignatureError:
-        # invalid signature
-        return None
+    except jwt.exceptions.InvalidSignatureError: return abort_or_validate(401, "token_failed_signaturecheck")
+    
+    valid_to_dttm = decoded_token.get('valid_to_dttm', None)
+    if valid_to_dttm is None: return abort_or_validate(401, "token_invalid_fields")
+    valid_to_dttm = dateutil.parser.parse(valid_to_dttm)
+    if valid_to_dttm < datetime.datetime.now(): return abort_or_validate(401, "token_expired")
+    
     #this_user = model.User.query.get()
     return decoded_token.get('user_id')
