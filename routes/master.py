9eba99a0a1aeced8f2245b2a06061b96c1c5f788
Cameron Harder-Hutton <cameron@harderhutton.dev>
2021-01-16 16:34:47 +1000

    Remove /api from start of routes

--- routes/master.py
+++ routes/master.py
@@ -6,25 +6,23 @@ from api_resources.venue_code import VenueCode
 from api_resources.token import Token
 from api_resources.user import User
 
-api.add_resource(User, "/api/user", "/api/user/<int:user_id>")
+api.add_resource(User, "/user", "/user/<int:user_id>")
 
 api.add_resource(
     Visit,
-    "/api/visit",
-    "/api/visit/<string:visit_id>",
-    "/api/venue/<int:venue_id>/visit",
-    "/api/venue/<int:venue_id>/venuecode/<string:venue_code_id>/visit",
+    "/visit",
+    "/visit/<string:visit_id>",
+    "/venue/<int:venue_id>/visit",
+    "/venue/<int:venue_id>/venuecode/<string:venue_code_id>/visit",
 )
 
-api.add_resource(
-    Venue, "/api/venue/<int:venue_id>", "/api/user/<int:user_id>/venue", "/api/venue"
-)
+api.add_resource(Venue, "/venue/<int:venue_id>", "/user/<int:user_id>/venue", "/venue")
 
 api.add_resource(
     VenueCode,
-    "/api/venue/<int:venue_id>/venuecode",
-    "/api/venue/<int:venue_id>/venuecode/<string:venue_code_id>",
-    "/api/venuecode/<string:venue_code_id>",
+    "/venue/<int:venue_id>/venuecode",
+    "/venue/<int:venue_id>/venuecode/<string:venue_code_id>",
+    "/venuecode/<string:venue_code_id>",
 )
 
-api.add_resource(Token, "/api/token")
+api.add_resource(Token, "/token")
